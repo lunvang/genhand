114042443d5809c62c1daae307a95087c7296aab
Lun Surdyaev <lunvang@gmail.com>
2023-02-25 17:03:36 +0200

    Refactored physics code

--- Assets/Scripts/shotgun.cs
+++ Assets/Scripts/shotgun.cs
@@ -7,7 +7,11 @@ public class shotgun : MonoBehaviour
     public Rigidbody2D rigidBody;
     public Transform cam;
 
-    private float shotPower = 20;
+    private float knockbackPower = 800;
+    private float recoilPower = 17;
+    private float camTorque = 10;
+    private float longRangeCamTorque = 3;
+    private float camTorqueCutoff = 15;
     private Vector3 initCamPosition = new Vector3(0, 0, -3);
 
     // Start is called before the first frame update
@@ -25,6 +29,7 @@ public class shotgun : MonoBehaviour
 
     private void FixedUpdate()
     {
+        resetRotation();
         pointToMouse();
     }
 
@@ -36,38 +41,43 @@ public class shotgun : MonoBehaviour
     void listenToEvents()
     {
         if (Input.GetMouseButtonDown(0))
-        {
-            float recoilDirection = (rigidBody.rotation + 180) * Mathf.Deg2Rad;
-            rigidBody.velocity += new Vector2(
-                Mathf.Cos(recoilDirection) * shotPower,
-                Mathf.Sin(recoilDirection) * shotPower
-            );
-        }
+            shoot();
+    }
+
+    void shoot()
+    {
+        // knockback
+        float knockbackDirection = (rigidBody.rotation + 180) * Mathf.Deg2Rad;
+        rigidBody.AddForce(
+            new Vector2(Mathf.Cos(knockbackDirection), Mathf.Sin(knockbackDirection)) * knockbackPower
+        );
+        // recoil
+        float recoilStrength = recoilPower * rigidBody.inertia;
+        rigidBody.AddTorque(recoilStrength, ForceMode2D.Impulse);
+    }
+
+    void resetRotation() // if rotation is more than full circle, substract 360
+    {
+        if (Mathf.Abs(rigidBody.rotation) > 360)
+            rigidBody.rotation = rigidBody.rotation - (360 * Mathf.Sign(rigidBody.rotation));
     }
 
     void pointToMouse()
     {
+        // calculate angle difference between desired angle and current angle
         Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 mousePos = Input.mousePosition - objectPos;
         float targetAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        if (Mathf.Abs(rigidBody.rotation) > 360)
-        {
-            rigidBody.rotation = (Mathf.Abs(rigidBody.rotation) - 360) * Mathf.Sign(rigidBody.rotation);
-        }
         float objectAngle = rigidBody.rotation;
         float angleDiff = targetAngle - objectAngle;
+
+        // if the angle is over 180 degrees, flip it
         if (Mathf.Abs(angleDiff) > 180)
-        {
-            angleDiff = (Mathf.Abs(angleDiff) - 360) * Mathf.Sign(angleDiff);
-        }
-        if (Mathf.Abs(angleDiff) > 90)
-        {
-            angleDiff = 90 * Mathf.Sign(angleDiff);
-        } else if (Mathf.Abs(angleDiff) < 15)
-        {
-            angleDiff *= 2;
-        }
-        float impulse = (angleDiff * Mathf.Deg2Rad * 5) * rigidBody.inertia;
+            angleDiff = angleDiff - (360 * Mathf.Sign(angleDiff));
+
+        // calculate impulse and apply it
+        float impulseStrength = (angleDiff < camTorqueCutoff) ? camTorque : longRangeCamTorque;
+        float impulse = angleDiff * Mathf.Deg2Rad * impulseStrength * rigidBody.inertia;
         rigidBody.AddTorque(impulse, ForceMode2D.Impulse);
     }
 }
